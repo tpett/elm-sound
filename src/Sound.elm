0b5359e2aa51c9217055e54cc7410b52848cfa70
Travis Petticrew <travis@petticrew.net>
2016-02-20 16:27:55 -0600

    Working sound wavelength calculator

---
+++ src/Sound.elm
@@ -0,0 +1,67 @@
+module Sound where
+
+import StartApp
+import Task exposing (Task)
+import Signal exposing (Signal, Address)
+import Effects exposing (Effects, Never)
+import Html exposing (Html, Attribute, div, input, label, text)
+import Html.Attributes exposing (id, for, value)
+import Html.Events exposing (on, targetValue)
+import String
+
+--
+-- StartApp boilerplate
+--
+app =
+  StartApp.start { init = init, view = view, update = update, inputs = [] }
+
+main : Signal Html
+main =
+  app.html
+
+port tasks : Signal (Task Never ())
+port tasks =
+  app.tasks
+
+--
+-- My type declarations
+--
+type alias Model = Int
+
+type Action
+  = NoOp
+  | FreqUpdate String
+
+--
+-- My functions
+--
+init : (Model, Effects Action)
+init = (1000, Effects.none)
+
+freqToWavelength : Int -> Float
+freqToWavelength freq =
+  345.41 / (toFloat freq)
+
+update : Action -> Model -> (Model, Effects Action)
+update action model =
+  case action of
+    NoOp -> (model, Effects.none)
+    FreqUpdate freqString ->
+      let
+          intResult = String.toInt(freqString)
+      in
+         case intResult of
+           Ok value -> (value, Effects.none)
+           Err msg -> (model, Effects.none)
+
+onInput : Signal.Address a -> (String -> a) -> Attribute
+onInput address contentToValue =
+  on "input" targetValue (\str -> Signal.message address (contentToValue str) )
+
+view : Address Action -> Model -> Html
+view address model =
+  div []
+    [ label [ (for "freq") ] [ text "Frequency" ]
+    , input [ (id "freq"), (value (toString model)), onInput address FreqUpdate ] []
+    , div [] [ text (toString (freqToWavelength model)) ]
+    ]
